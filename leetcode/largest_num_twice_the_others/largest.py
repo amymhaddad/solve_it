0ce5ba987fa8684a1dab714d58a5416fd078bce6
Amy M Haddad <amy.m.haddad@gmail.com>
2020-02-10 11:18:22 -0500

    Completed largest num and twice the size

--- leetcode/largest_num_twice_the_others/largest.py
+++ leetcode/largest_num_twice_the_others/largest.py
@@ -1,19 +1,46 @@
-numbers = [3, 6, 1, 0]
+"""
+In a given integer array nums, there is always exactly one largest element.
 
-def get_number(numbers):
-    
+Find whether the largest element in the array is at least twice as much as every other number in the array.
+
+If it is, return the index of the largest element, otherwise return -1.
+
+Example 1:
+
+Input: nums = [3, 6, 1, 0]
+Output: 1
+"""
+
+
+def get_number_approach_1(numbers):
+
     max_number = max(numbers)
-    
-    numbers_values = {}
-    for i, num in enumerate(numbers):
-        # import pdb; pdb.set_trace()
-        if max_number <= num * 2:
-            return -1
-        else:
-            if max_number == num:
-                numbers_values[max_number] = i
-    print(numbers_values)
-        
-        
 
-print(get_number(numbers))
+    doubled_nums = []
+    for num in numbers:
+        if num != max_number:
+            doubled_nums.append(num * 2)
+
+    if max_number >= max(doubled_nums):
+        return numbers.index(max_number)
+    return -1
+
+
+def get_number_approach_2(numbers):
+
+    max_number = max(numbers)
+
+    if all(max_number >= num * 2 for num in numbers if num != max_number):
+        return numbers.index(max_number)
+    return -1
+
+
+def get_number_approach_3(numbers):
+    max_number = max(numbers)
+
+    for num in numbers:
+        if num != max_number and num * 2 > max_number:
+
+            return -1
+
+    return numbers.index(max_number)
--- leetcode/largest_num_twice_the_others/test_largest.py
+++ leetcode/largest_num_twice_the_others/test_largest.py
@@ -1,5 +1,16 @@
+from largest import get_number_approach_1, get_number_approach_2, get_number_approach_3
 
-from largest import get_number
 
 def test_number_is_largest_and_twice_size():
-    assert get_number([3, 6, 1, 0]) == 1
+
+    assert get_number_approach_1([3, 6, 1, 0]) == 1
+    assert get_number_approach_1([1, 2, 3, 4]) == -1
+    assert get_number_approach_1([0, 0, 0, 1]) == 3
+
+    assert get_number_approach_2([3, 6, 1, 0]) == 1
+    assert get_number_approach_2([1, 2, 3, 4]) == -1
+    assert get_number_approach_2([0, 0, 0, 1]) == 3
+
+    assert get_number_approach_3([3, 6, 1, 0]) == 1
+    assert get_number_approach_3([1, 2, 3, 4]) == -1
+    assert get_number_approach_3([0, 0, 0, 1]) == 3
