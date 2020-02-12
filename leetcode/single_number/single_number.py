23b2d3ced3610d6c8eed37db0bfdae3ed840e601
Amy M Haddad <amy.m.haddad@gmail.com>
2020-02-12 11:09:45 -0500

    WIP

--- leetcode/single_number/single_number.py
+++ leetcode/single_number/single_number.py
@@ -7,11 +7,19 @@ def find_single_number(nums):
     sorted_nums = sorted(nums)
     i = 0
 
+    n = []
     for num in sorted_nums:
-        unique_nums[num] = i
-        i += 1    
-    return unique_nums
-        
+        next_num = sorted_nums[(i+ 1) % len(sorted_nums)]
 
+        if sorted_nums[i] == next_num:
+            # import pdb; pdb.set_trace()
+            i += 1
+            continue
+
+        elif i == len(sorted_nums) -1:
+            print(sorted_nums[i])
+
+    
+ 
 
 print(find_single_number(nums))
