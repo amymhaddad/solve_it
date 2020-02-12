9018018fd7542b51c3fce4680bb1b976025c3089
Amy M Haddad <amy.m.haddad@gmail.com>
2020-02-12 17:40:53 -0500

    Completed problem

--- leetcode/single_number/single_number.py
+++ leetcode/single_number/single_number.py
@@ -4,22 +4,24 @@ nums = [4,1,2,1,2]
 def find_single_number(nums):
     unique_nums = {}
 
-    sorted_nums = sorted(nums)
-    i = 0
+    for i, num in enumerate(nums):
+        unique_nums[num] = unique_nums.get(num, 0) + 1
 
-    n = []
-    for num in sorted_nums:
-        next_num = sorted_nums[(i+ 1) % len(sorted_nums)]
+    for k, v in unique_nums.items():
+        if unique_nums[k] == 1:
+            return k
+print(find_single_number(nums))
 
-        if sorted_nums[i] == next_num:
-            # import pdb; pdb.set_trace()
-            i += 1
-            continue
 
-        elif i == len(sorted_nums) -1:
-            print(sorted_nums[i])
+#   for num in sorted_nums:
+#         next_num = sorted_nums[(i+ 1) % len(sorted_nums)]
+
+#         if sorted_nums[i] == next_num:
+#             # import pdb; pdb.set_trace()
+#             i += 1
+#             continue
+
+#         elif i == len(sorted_nums) -1:
+#             print(sorted_nums[i])
 
     
- 
-
-print(find_single_number(nums))
