c9b1949a7e2e1ae203d960f7afa40620e2ae65f2
Amy M Haddad <amy.m.haddad@gmail.com>
2020-04-04 10:18:16 -0400

    Completed reverse string

--- interview_cake/reverse_string/reverse_string.py
+++ interview_cake/reverse_string/reverse_string.py
@@ -1,31 +1,25 @@
+"""
+Write a function that takes a list of characters and reverses the letters in place.
+"""
+
 list_string = ["A", "B", "C", "D"]
 
 
 def reverse(list_string):
 
-    left = -1
+    left = len(list_string) - 1
     right = 0
 
     if len(list_string) <= 1:
         return list_string
 
-    while True:
-        # import pdb
+    while right < left:
 
-        # pdb.set_trace()
-        # Update this test to account for when all letters ahve been reversed and only have a middle char
-        if left + right <= 1:
-            break
+        list_string[left], list_string[right] = (
+            list_string[right],
+            list_string[left],
+        )
 
-        elif len(list_string) // 2 == right:
-            break
-
-        else:
-            list_string[left], *_, list_string[right] = list_string
-
-            right += 1
-            left -= 1
+        right += 1
+        left -= 1
     return list_string
-
-
-print(reverse(list_string))
