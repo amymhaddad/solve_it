264adfced63e6a4f062563511671a63298e958be
Amy M Haddad <amy.m.haddad@gmail.com>
2020-04-16 11:13:37 -0400

    removed old comments from stack file

--- interview_cake/max_stack/stack.py
+++ interview_cake/max_stack/stack.py
@@ -22,29 +22,3 @@ class Stack(object):
 
     def is_empty(self):
         return bool(len(self.items) == 0)
-
-
-#   def push(self, item):
-#         """If maxes_stack is empty, then add item to max_stack.
-#         If item is larger than the topmost item in maxes_stack, then remove the current max number.
-#         Add item to max_stack.
-#         """
-
-#         if self.maxes_stack.is_empty():
-#             self.maxes_stack.push(item)
-
-#         elif item > self.maxes_stack.peek():
-#             self.maxes_stack.pop()
-#             self.maxes_stack.push(item)
-
-#     def pop(self):
-#         """Remove and return the top item from our stack."""
-#         self.pop()
-
-#     def peek(self):
-#         """Return the last item without removing it"""
-#         return self.maxes_stack.peek()
-
-#     def get_max(self):
-#         """Get largest number in stack"""
-#         return self.maxes_stack.peek()
