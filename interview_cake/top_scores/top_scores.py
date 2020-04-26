e9c1360d31880efae4d55644976da9ce81aed438
Amy M Haddad <amy.m.haddad@gmail.com>
2020-04-26 11:38:41 -0400

    Added comments to file

--- interview_cake/top_scores/top_scores.py
+++ interview_cake/top_scores/top_scores.py
@@ -22,18 +22,26 @@ def sort_scores(unsorted_scores, highest_possible_score):
             
     return [scores]
 
+#See if the score is in the range of numbers. IF it is, then iterate through the nubmer of times it's included
+
 # nums = [4, 8, 4, 2]
 
 # counts = []
 
-# for i, num in enumerate(nums):
-#     repeats = nums.count(num)
-#     if num in counts:
-#         continue
-#     counts.insert(num-1, repeats)
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+#Create an array with values set to 0
+#My index is the key in an array -- it is still a hash
+# >>> counts = 100*[0]
 
-# print(counts)
+#At postion 50 increment the value 1 
+# >>> counts[50] += 1
 
+# >>> counts[20] += 1
+# >>> counts[60] += 1
+# >>> counts
+# [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+# >>> counts[60] += 1
+# >>> counts
 
 
 
