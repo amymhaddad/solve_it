47d53923f0e7df7128d8f53715d0cac496a0319e
Amy M Haddad <amy.m.haddad@gmail.com>
2020-08-11 12:13:56 -0400

    Solved it.

---
+++ daily_coding_problem/string_mapping/string_mapping.py
@@ -0,0 +1,23 @@
+"""
+Determine whether there exists a one-to-one character mapping from one string s1 to another s2.
+
+For example, given s1 = abc and s2 = bcd, return true since we can map a to b, b to c, and c to d.
+
+Given s1 = foo and s2 = bar, return false since the o cannot map to two characters.
+"""
+
+s1 = "foo"
+s2 = "bar"
+
+
+def string_mapping(s1, s2):
+    if len(s1) != len(s2):
+        return False
+
+    set_length_s1 = len(set(s1))
+    set_length_s2 = len(set(s2))
+
+    return set_length_s1 == set_length_s2
+
+
+print(string_mapping(s1, s2))
