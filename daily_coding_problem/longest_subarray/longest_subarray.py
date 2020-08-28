d7648a64d6d10cdc7838faab061ec5c7449f52d8
Amy M Haddad <amy.m.haddad@gmail.com>
2020-08-28 11:30:11 -0400

    first commit

--- daily_coding_problem/longest_subarray/longest_subarray.py
+++ daily_coding_problem/longest_subarray/longest_subarray.py
@@ -4,14 +4,6 @@ Given an array of elements, return the length of the longest subarray where all 
 For example, given the array [5, 1, 3, 5, 2, 3, 4, 1], return 5 as the longest subarray of distinct elements is [5, 2, 3, 4, 1].
 """
 
-arr = [5, 1, 2, 2, 2]
-
-<<<<<<< HEAD
-=======
-arr = [5, 1, 2, 2, 2]
-
->>>>>>> b11d97d... Solved it
-
 from collections import defaultdict
 
 
