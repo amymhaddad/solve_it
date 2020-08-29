6b8fbcbc7e9b889f1b5609ae47aac78f33d8e163
Amy M Haddad <amy.m.haddad@gmail.com>
2020-08-29 10:08:35 -0400

    WIP

--- leetcode/smallerNumbersThanCurrent/smaller.py
+++ leetcode/smallerNumbersThanCurrent/smaller.py
@@ -1,36 +1,47 @@
 nums = [6, 5, 4, 8]
 
-# k = 1
-# for i, num in enumerate(nums):
-#     y = nums[(i + k) % len(nums)]
-#     print(y)
 
-
-from collections import deque
+from collections import deque, Counter
 
 num_deque = deque(nums)
-# num_deque.rotate(1)
-# print(num_deque)
 
+
+# i = 0
+
+
+# while i <= len(nums) - 1:
+#     num_deque.rotate(-i)
+#     print(num_deque)
+
+#     i += 1
+
+
+# rotated_list = []
+# for i, num in enumerate(nums):
+#     rotated_index = (i + 1) % len(nums)
+#     rotated_list.append(nums[rotated_index])
+# print(rotated_list)
+
+# num = 0
+# while num <= len(nums):
+#     print(num)
+#     each_list = []
+#     for i, num in enumerate(nums):
+#         rotated_index = (i + 1) % len(nums)
+#         each_list.append(nums[rotated_index])
+#     num += 1
+#     rotated_list.append(each_list)
+
+# print(rotated_list)
 i = 0
 
-while i <= len(nums) - 1:
-    num_deque.rotate(-i)
-    print(num_deque)
+while i < len(nums):
+    each_iteration = Counter()
+    for num in num_deque:
+        each_iteration[num]
+    x = num_deque.popleft()
+    num_deque.append(x)
+
     i += 1
-
-
-# def rotate(nums, k):
-#     nums_length = len(nums)
-
-#     rotated_array = [None] * nums_length
-
-#     for i, num in enumerate(nums):
-#         rotated_index = (i + k) % nums_length
-
-#         rotated_array[rotated_index] = num
-
-#     for i in range(nums_length):
-#         nums[i] = rotated_array[i]
-#    return nums
+    print(each_iteration)
 
