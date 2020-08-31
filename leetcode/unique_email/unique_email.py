8c45584c698f4d40d23db34a6ca78ac57bf914b9
Amy M Haddad <amy.m.haddad@gmail.com>
2020-08-31 12:08:43 -0400

    First commit

---
+++ leetcode/unique_email/unique_email.py
@@ -0,0 +1,16 @@
+# Create a list of strings split on "@", so I have a list that contains the name and domain name ["alice", "leetcode.com"]
+# create an empty dictionary to hold all unique email addresses
+
+# Validate the name
+# Check to see if name is in the dictionary. If not, join the strings together on teh "," with "@" and add it to the dictionary
+# Otherwise, I need validate the name
+# Check if there are "." If so remove them  --> use regex
+# Check if there are "+" If so extract the name up to the "+"
+# Save the parsed name as a variable
+
+# Now see if the parsed name is a value in the dictionary via this syntax:
+# if dictionary[name] -- or use setfault
+# If parsed name is not in the dictioanry, add it
+
+# Return the length of the dictionary
+
