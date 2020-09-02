d0a5a77d043723cb4fc4373cea2d88837ecf1be9
Amy M Haddad <amy.m.haddad@gmail.com>
2020-09-02 13:31:50 -0400

    Added Cage class

--- oop/zoo/zoo.py
+++ oop/zoo/zoo.py
@@ -18,5 +18,52 @@ class Wolf(Animal):
         self.legs = 4
 
 
-wolf = Wolf("black")
-print(wolf)
+class Sheep(Animal):
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+        self.legs = 4
+
+
+class Snake(Animal):
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+        self.legs = 0
+
+
+class Parrot(Animal):
+    def __init__(self, color):
+        super().__init__()
+        self.color = color
+        self.legs = 2
+
+
+wolf = Wolf("black")  # species, color, # legs
+sheep1 = Sheep("white")
+sheep2 = Sheep("white")
+snake = Snake("brown")
+parrot = Parrot("black")
+
+
+class Cage(object):
+    def __init__(self, id):
+        self.id = id
+        self.cage = []
+
+    def add_animals(self, *animals):
+        for animal in animals:
+            self.cage.append(animal)
+
+    def __repr__(self):
+
+        output = f"Cage {self.id}:\n"
+        output += "\n".join(
+            [f"\t{i} {animal}" for i, animal in enumerate(self.cage, 1)]
+        )
+        return output
+
+
+c2 = Cage(2)
+c2.add_animals(snake, parrot)
+print(c2)
