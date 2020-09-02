8db0012aae2db4311e46749a13e489b49774a79b
Amy M Haddad <amy.m.haddad@gmail.com>
2020-09-02 14:15:35 -0400

    Solved it

--- oop/zoo/study.py
+++ oop/zoo/study.py
@@ -1,47 +1,53 @@
+
 # Key point: just create what you need
 # Put all the stuff that's common to all classes into this parent class
-class Species:
-    def __init__(self, color):
-        self.color = color
-        # Get the name of the species without invoking another var via self.className.nameofclass
-        self.name = self.__class__.__name__
+# Use inheritance, since a wolf IS A species, and pass in what's different
+
+
+class Animal(object):
+    def __init__(self):
+        self.animal = self.__class__.__name__.lower()
 
     def __repr__(self):
-        return f"{self.color} {self.species} {self.legs}"
+        return f"{self.color} {self.animal} {self.legs}"
 
 
-# Use inheritance, since a wolf IS A species, and pass in what's different
-class Wolf(Species):
+class Wolf(Animal):
     def __init__(self, color):
-        super().__init__(color)
-        self.species = "wolf"
+        super().__init__()
+        self.color = color
         self.legs = 4
 
 
-class Sheep(Species):
+class Sheep(Animal):
     def __init__(self, color):
-        super().__init__(color)
-        self.species = "sheep"
+        super().__init__()
+        self.color = color
         self.legs = 4
 
 
-class Snake(Species):
+class Snake(Animal):
     def __init__(self, color):
-        super().__init__(color)
-        self.species = "snake"
+        super().__init__()
+        self.color = color
         self.legs = 0
 
 
-class Parrot(Species):
+class Parrot(Animal):
     def __init__(self, color):
-        super().__init__(color)
-        self.species = "parrot"
+        super().__init__()
+        self.color = color
         self.legs = 2
 
 
-class Cage(object):
-    total_animals = 0
+wolf = Wolf("black")  # species, color, # legs
+sheep1 = Sheep("white")
+sheep2 = Sheep("white")
+snake = Snake("brown")
+parrot = Parrot("black")
 
+
+class Cage(object):
     def __init__(self, id):
         self.id = id
         self.cage = []
@@ -49,60 +55,77 @@ class Cage(object):
     def add_animals(self, *animals):
         for animal in animals:
             self.cage.append(animal)
-        return self.cage
+
+    # This method is invoked from the Zoo class. When it's caleed, I iterate through self.cage
+    # For each animal in the cage, I get check the color (which was passed in from Zoo)
+    # I return the animals that match the arg that's passed in
+    #This works bc each Cage has an animal class, which inherits from the Species() class to get the attributes 
+    def animals_by_color(self, color):
+        return "\n".join([str(animal) for animal in self.cage if animal.color == color])
 
     def __repr__(self):
-
-        output = f"Cage {self.id}\n"
+        output = f"Cage {self.id}:\n"
         output += "\n".join(
-            ["\t" + f"{i} {animal}" for i, animal in enumerate(self.cage, 1)]
+            [f"\t{i} {animal}" for i, animal in enumerate(self.cage, 1)]
         )
         return output
-        # output = ""
-        # for animal in self.cage:
-        #     Cage.total_animals += 1
-        #     animal_details = f"{Cage.total_animals} {str(animal)} legs"
-        #     output += animal_details + "\n"
-        # return output
 
-
-class Zoo:
-    def __init__(self):
-        self.new_cage = []
-
-    def add_cages(self, *cages):
-        for cage in cages:
-            self.new_cage.append(cage)
-
-    # def get_animals_by_color(self, color):
-    #     for animal in self.new_cage:
-    #         print("HERE", animal)
-
-    def __repr__(self):
-        return "\n".join([str(cage) for cage in self.new_cage])
-
-
-wolf = Wolf("black")  # species, color, # legs
-
-sheep1 = Sheep("white")
-sheep2 = Sheep("white")
-snake = Snake("brown")
-parrot = Parrot("black")
-
-# print(wolf)  #  black wolf, 4 legs
-# print(sheep1)  #  white sheep, 4 legs
-# print(sheep2)  #  white sheep, 4 legs
 
 c1 = Cage(1)  # ID numbers
 c1.add_animals(wolf, sheep1, sheep2)
 
-# print(c1)
 
 c2 = Cage(2)
 c2.add_animals(snake, parrot)
 
-# print(c2)
+
+class Zoo(object):
+    def __init__(self):
+        self.total_cages = []
+
+    def add_cages(self, *cages):
+        for cage in cages:
+            self.total_cages.append(cage)
+
+    # Option 1: Get animals by color:
+    # Iterate through self.total_cages in this class.
+    # THEN on each iteration call the class of the same name in the Cage() class and pass in the color to that class
+    def animals_by_color(self, color):
+        return "\n".join(
+            [
+                # Note: instead of self, I use the curent iteration of my iteratble, one_cage
+                one_cage.animals_by_color(color)
+                for one_cage in self.total_cages
+            ]
+        )
+
+    # Alt to to the above method
+    # First, I iterate through self.total_cages from this class.
+    # Second, I iterate through each cage in the Cage class and access its atribute, cage
+    # for one_animal in one_cage.cage --> cage is the attribute in the Cage() class
+    # Then I add my conditional
+    # This method prevents me from having to make the extra method in the Cage class
+    def animals_by_color2(self, color):
+        return "\n".join(
+            [
+                str(one_animal)
+                for one_cage in self.total_cages
+                for one_animal in one_cage.cage
+                if one_animal.color == color
+            ]
+        )
+
+    def number_of_legs(self):
+        return sum([animal.legs for cages in self.total_cages for animal in cages.cage])
+
+    def __repr__(self):
+        output = ""
+        output += "".join(f"{cage}\n" for cage in self.total_cages)
+        return output
+
+
 z = Zoo()
 z.add_cages(c1, c2)
 
-print(z.get_animals_by_color("black"))
+print(z.animals_by_color2("black"))
+print(z.number_of_legs())
