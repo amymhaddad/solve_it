a92df5426ffc4653daf6d7461cda575038489e34
Amy M Haddad <amy.m.haddad@gmail.com>
2020-09-03 11:57:42 -0400

    Update to algorithm

--- leetcode/morse_code/morse_code.py
+++ leetcode/morse_code/morse_code.py
@@ -1,34 +1,32 @@
 from string import ascii_lowercase as lower
 
 MORSE_CODE_REPRESENTATIONS = [
-    [
-        ".-",
-        "-...",
-        "-.-.",
-        "-..",
-        ".",
-        "..-.",
-        "--.",
-        "....",
-        "..",
-        ".---",
-        "-.-",
-        ".-..",
-        "--",
-        "-.",
-        "---",
-        ".--.",
-        "--.-",
-        ".-.",
-        "...",
-        "-",
-        "..-",
-        "...-",
-        ".--",
-        "-..-",
-        "-.--",
-        "--..",
-    ]
+    ".-",
+    "-...",
+    "-.-.",
+    "-..",
+    ".",
+    "..-.",
+    "--.",
+    "....",
+    "..",
+    ".---",
+    "-.-",
+    ".-..",
+    "--",
+    "-.",
+    "---",
+    ".--.",
+    "--.-",
+    ".-.",
+    "...",
+    "-",
+    "..-",
+    "...-",
+    ".--",
+    "-..-",
+    "-.--",
+    "--..",
 ]
 
 
@@ -36,20 +34,11 @@ def unique_morse_code_representations(words):
 
     letter_to_morse_code = {
         letter: morse_code
-        for letters in MORSE_CODE_REPRESENTATIONS
-        for letter, morse_code in zip(lower, letters)
+        for letter, morse_code in zip(list(lower), MORSE_CODE_REPRESENTATIONS)
     }
+    seen = {"".join(letter_to_morse_code[letter] for letter in word) for word in words}
 
-    unique_transformations = set()
-    each_transformation = ""
-
-    for word in words:
-        for letter in word:
-            each_transformation += letter_to_morse_code[letter]
-        unique_transformations.add(each_transformation)
-        each_transformation = ""
-
-    return len(unique_transformations)
+    return len(seen)
 
 
 print(unique_morse_code_representations(["gin", "zen", "gig", "msg"]))
