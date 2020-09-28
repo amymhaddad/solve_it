1443cd066e878c0a482a499c82636f01534b7b90
Amy M Haddad <amy.m.haddad@gmail.com>
2020-09-28 12:19:15 -0400

    On last part of problem

--- exercism/python/crypto-square/crypto_square.py
+++ exercism/python/crypto-square/crypto_square.py
@@ -40,25 +40,27 @@ rectangle = create_rectangle(chars)
 
 def parse_rectangle(rectangle):
 
-    each_row = ""
+    each_row = []
     start = 0
     end = rectangle["column"]
 
     while end <= len(chars):
-        each_row += chars[start:end] + "\n"
+        for letter in chars[start:end]:
+            each_row.append(letter + ",")
+        each_row.append("\n")
         start = end
         end += rectangle["column"]
 
-    return each_row.strip()
+    return "".join(each_row)
 
 
-print(parse_rectangle(rectangle))
+# ['t,', 'h,', 'i,', '\n', 's,', 'i,', 's,', '\n', 'f,', 'u,', 'n,', '\n']
+
+# Iterate through the string; spilit on \n 
+#Use zip to grab each col 
+parsed_rectangle = parse_rectangle(rectangle)
 
 
-# for i in range(0, len(chars), rectangle["column"]):
-#     print(i)
-#     each_row += chars[: chars[i]] + "\n"
-# print(each_row)
+# each_row = [[chars] for rows in parsed_rectangle.split("\n") for chars in rows]
 
-# print(f"{chars[word]}\n")
-
+print(parsed_rectangle)
