815e42ad5cf178a1d9152e7c971c8630016588c2
Amy M Haddad <amy.m.haddad@gmail.com>
2020-09-29 12:07:34 -0400

    WIP accommodating for spaces

--- exercism/python/crypto-square/crypto_square_test.py
+++ exercism/python/crypto-square/crypto_square_test.py
@@ -31,12 +31,12 @@ class CryptoSquareTest(unittest.TestCase):
         expected = "tsf hiu isn"
         self.assertEqual(cipher_text(value), expected)
 
-    # def test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space(
-    #     self
-    # ):
-    #     value = "Chill out."
-    #     expected = "clu hlt io "
-    #     self.assertEqual(cipher_text(value), expected)
+    def test_8_character_plaintext_results_in_3_chunks_the_last_one_with_a_trailing_space(
+        self,
+    ):
+        value = "Chill out."
+        expected = "clu hlt io "
+        self.assertEqual(cipher_text(value), expected)
 
     # def test_54_character_plaintext_results_in_7_chunks_the_last_two_with_trailing_spaces(
     #     self
