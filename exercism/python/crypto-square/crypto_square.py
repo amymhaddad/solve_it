744958470bacd2459a1820233e1bb0eb565409a2
Amy M Haddad <amy.m.haddad@gmail.com>
2020-10-01 10:08:25 -0400

    Functions work -- need to debug

--- exercism/python/crypto-square/crypto_square.py
+++ exercism/python/crypto-square/crypto_square.py
@@ -41,16 +41,9 @@ def parse_rectangle(rectangle, chars):
     return total_rows
 
 
-# rectangle_with_parsed_words = parse_rectangle(rectangle)
-#  self.rows = [[int(x) for x in xs.split()] for xs in M.splitlines()]
-#         self.cols = [col for col in zip(*self.rows)]
-
-
 def encoded_words(parsed_rectangle):
     cols = [list(col) for col in zip(*parsed_rectangle)]
-    print(cols)
-
-    # x = "".join(["".join(word) + " " for word in cols]).strip()
+    return " ".join(["".join(word) + " " for word in cols])
 
 
 # encode = encoded_words(rectangle_with_parsed_words)
@@ -61,38 +54,13 @@ def cipher_text(plain_text):
     else:
         chars = normalize_string(plain_text)
         rectangle = create_rectangle(chars)
-        print(rectangle)
         rectangle_with_parsed_words = parse_rectangle(rectangle, chars)
         encode = encoded_words(rectangle_with_parsed_words)
         return encode
 
 
-print(
-    cipher_text(
-        "If man was meant to stay on the ground, god would have given us roots."
-    )
-)
-
-
-# cols = [list(col) for col in zip(*self.rows)]
-
-
-class Matrix(object):
-    def __init__(self, M):
-        # self.rows = [[int(x) for x in xs.split()] for xs in M.splitlines()]
-        self.rows = [
-            ["i, f, m, a, n, w, a, s, "],
-            ["m, e, a, n, t, t, o, s, "],
-            ["t, a, y, o, n, t, h, e, "],
-            ["g, r, o, u, n, d, g, o, "],
-            ["d, w, o, u, l, d, h, a, "],
-            ["v, e, g, i, v, e, n, u, "],
-        ]
-        self.cols = [col for col in zip(*self.rows)]
-
-
-matrix = "89 1903 3\n18 3 1\n9 4 800"
-# m = Matrix(matrix)
-# print(m.rows)
-# print(m.cols)
-# # [[89, 1903, 3], [18, 3, 1], [9, 4, 800]]
+# print(
+#     cipher_text(
+#         "If man was meant to stay on the ground, god would have given us roots."
+#     )
+# )
