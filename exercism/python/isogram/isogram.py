815fa135d7eeb3344c767005cc26bbaa0943118f
Amy M Haddad <amy.m.haddad@gmail.com>
2020-11-02 11:42:46 -0500

    Update to program

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -2,12 +2,5 @@ import re
 
 
 def is_isogram(string):
-
-    valid_chars = "".join(
-        [
-            char.lower()
-            for char in re.findall(r"[^- ]+", string.lower())
-            if char.isalpha()
-        ]
-    )
-    return len(valid_chars) == len(set(valid_chars))
+    letters = re.sub(r"[- ]+", "", string.lower())
+    return len(letters) == len(set(letters))
