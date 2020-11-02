388e376b132ed5caf0cae57d3023463d394f30fe
Amy M Haddad <amy.m.haddad@gmail.com>
2020-11-02 11:31:24 -0500

    Update variable name

--- exercism/python/isogram/isogram.py
+++ exercism/python/isogram/isogram.py
@@ -3,11 +3,11 @@ import re
 
 def is_isogram(string):
 
-    valid = "".join(
+    valid_chars = "".join(
         [
             char.lower()
             for char in re.findall(r"[^- ]+", string.lower())
             if char.isalpha()
         ]
     )
-    return len(valid) == len(set(valid))
+    return len(valid_chars) == len(set(valid_chars))
