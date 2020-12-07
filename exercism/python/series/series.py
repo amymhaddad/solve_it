1d1ab10695829b0014854348d833693c1f2a164d
Amy M Haddad <amy.m.haddad@gmail.com>
2020-12-07 11:08:01 -0500

    Solved it

---
+++ exercism/python/series/series.py
@@ -0,0 +1,12 @@
+def slices(series, length):
+    if length > len(series) or length <= 0:
+        raise ValueError("Invalid length")
+    if length == len(series):
+        return [series]
+    substrings = []
+    i = 0
+    while length < len(series):
+        substrings.append(series[i:length])
+        i, length = i + 1, length + 1
+    substrings.append(series[i:length])
+    return substrings
