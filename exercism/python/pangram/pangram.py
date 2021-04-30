1d2def2a62258df6d5288ed54df51255526f657a
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-30 19:10:56 -0400

    2nd approach works

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -7,18 +7,32 @@ import re
 #         len(set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence))) == 26
 #     )
 
+#Ch
 #dictionary
 from string import ascii_lowercase as lower
+# def is_pangram(sentence):
+#     alphabet = {letter:0 for letter in lower}
+#
+#     for character in sentence.lower():
+#         if character.isalpha():
+#             alphabet[character] = alphabet.get(character, 0) + 1
+#     for letter, count in alphabet.items():
+#         if count == 0:
+#             return False
+#     return True
+#    
+#Sort and compare
 def is_pangram(sentence):
-    alphabet = {letter:0 for letter in lower}
+    if not sentence:
+        return False
 
-    for character in sentence.lower():
-        if character.isalpha():
-            alphabet[character] = alphabet.get(character, 0) + 1
-    for letter, count in alphabet.items():
-        if count == 0:
-            return False
-    return True
-    
+    phrase_letters = []
 
+    for letter in sentence.lower():
+        if letter.isalpha() and letter not in phrase_letters:
+            phrase_letters.append(letter)
 
+    phrase_letters.sort()
+    return phrase_letters == list(lower)
+#jsentence = "the quick brown fox jumps over the lazy dog"
+#print(is_pangram(sentence))
