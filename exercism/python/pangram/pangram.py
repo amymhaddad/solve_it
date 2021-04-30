eefe4ba7e896f91a7be6a9c891a2039d16c25795
Amy M Haddad <amy.m.haddad@gmail.com>
2021-04-30 11:52:49 -0400

    first commit

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -1,8 +1,19 @@
 import re
 
 
-def is_pangram(sentence):
+# def is_pangram(sentence):
+#
+#     return (
+#         len(set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence))) == 26
+#     )
 
-    return (
-        len(set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence))) == 26
-    )
+def is_pangram(sentence):
+    letter_counter = {}
+
+    for character in sentence:
+        if character.isalpha():
+            letter_counter[character] = letter_counter.get(character, 0) + 1
+
+    print(letter_counter)
+
+print(is_pangram("abceddd"))
