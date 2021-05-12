70456ed1f189de716c78567f65fcb694f961da05
Amy M Haddad <amy.m.haddad@gmail.com>
2021-05-12 08:49:08 -0400

    Refactoring solutions from PR

--- exercism/python/pangram/pangram.py
+++ exercism/python/pangram/pangram.py
@@ -1,25 +1,36 @@
 import re
 from string import ascii_lowercase as lowercase_letters
 
-# set
+# set - 1
 def is_pangram(sentence):
+    alphabet_length = 26
     return (
-        len(set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence))) == 26
+        len(set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence)))
+        == alphabet_length
     )
 
 
-# set()
+# set - 2
 def is_pangram(sentence):
+    return set(letter.lower() for letter in re.findall(r"[A-Za-z]", sentence)) == set(
+        lowercase_letters
+    )
 
-    alphabet_length = 26
+
+# set - 3
+def is_pangram(sentence):
     sentence_letters = set()
 
     for letter in sentence.lower():
         if letter.isalpha():
             sentence_letters.add(letter)
 
-    return len(sentence_letters) == alphabet_length
+    return len(sentence_letters) == len(lowercase_letters)
 
+#set - 4
+def is_pangram(sentence):
+    letters = re.findall(r"[A-Za-z]", sentence.lower())
+    return set(lowercase_letters).issubset(set(letters))
 
 # dictionary
 def is_pangram(sentence):
@@ -28,6 +39,7 @@ def is_pangram(sentence):
     for character in sentence.lower():
         if character.isalpha():
             alphabet[character] = alphabet.get(character, 0) + 1
+
     for letter, count in alphabet.items():
         if count == 0:
             return False
@@ -62,12 +74,11 @@ def is_pangram(sentence):
 # array v2
 def is_pangram(sentence):
     alphabet_length = 26
-    ascii_start = 97
     total_letters = [0 for i in range(alphabet_length)]
 
     for i, char in enumerate(sentence):
         if char.isalpha():
-            letter_index = ord(char.lower()) - ascii_start
+            letter_index = ord(char.lower()) - ord("a")
             total_letters[letter_index] = 1
 
     return sum(total_letters) == alphabet_length
